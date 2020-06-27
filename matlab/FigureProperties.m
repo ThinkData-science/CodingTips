0c234065195b95187fcd211d6bf122853485b66f
CJ Della Porta <cjdellaporta@gmail.com>
2020-06-26 21:17:49 -0400

    Adding a script to illustrate figure properties. Starting a new script for plotting tips (incomplete).

---
+++ matlab/FigureProperties.m
@@ -0,0 +1,66 @@
+%% Working with figure properties
+% This file introduces the set() and get() functions and explains how to use 
+% them to interact with figure properties. 
+%
+%%
+
+% Create a figure and save it's handle.
+f1 = figure();
+
+% Using the get() function we can examine the properties on the Figure. In 
+% MATLAB this will return a class, but in Octave this will return a struct.
+figureInfo = get( f1 );
+disp( figureInfo );
+
+% Rather than returning all of the properties, we can also request a specific 
+% property that we are interested in. For example, we can return the current
+% figure name by using the keyword, 'name', which will be an empty string by 
+% default. 
+figureName = get( f1, 'name' );
+disp( figureName );
+
+% Similarly, we can set properties on the figure using the set() method. For 
+% example, we can change the figure name with the following command.
+set( f1, 'name', 'Figure Properties Tutorial' );
+figureName = get( f1, 'name' );
+disp( figureName );
+
+% One of the most common properties you will want to use is 'position'. 
+% We can get the current figure position using this property as follows;
+pos = get( f1, 'position' );
+disp( pos );
+
+% The position property is a 1x4 vector that contains horizontal origin (h0), 
+% vertical origin (v0), width (w), and height (h), respectively. So it looks 
+% like this: [ h0, v0, w, h ]. (h0, v0) define where the bottom left corner of
+% the figure window will appear. (w,h) define the width and height of the 
+% figure in the current units. By default figures are defined in pixels. So
+% this example will place the figure in the bottom left corner of the screen 
+% and will be 300 pixels wide by 600 pixels tall.
+set( f1, 'position', [0, 0, 300, 600] );
+
+% Similarly, we can make a short and fat figure like so:
+set( f1, 'position', [0, 0, 600, 300] );
+
+% It is also possible to specify the figure position in normalized units 
+% (scaled between 0 and 1) and inches using the "units" property and the 
+% keywords "normalized" and "inches", respectively. Both are useful in 
+% different cases. You can change the units with the set() command. Here is an 
+% example that will create a figure that takes up the left half of the screen.
+set( f1, 'units', 'normalized' );
+set( f1, 'position', [0, 0, 0.5, 1] ); 
+
+% Here is an example that will create a figure that is 3 inches wide by 4
+% inches tall. 
+set( f1, 'units', 'inches' );
+set( f1, 'position', [0, 0, 3, 4] ); 
+
+% The set() and get() functions are extremly useful and can be used to adjust 
+% the properties of many different objects. It is important to note that 
+% MathWorks introduced the HG2 Graphics system starting in MATLAB R2014b which
+% allows you to directly access graphic objects using the dot operator. The 
+% set() and get() methods still work, however, The interested reader is referred
+% to the MathWorks webpage for more information. 
+% ("https://www.mathworks.com/products/matlab/matlab-graphics.html" )
+
+%% end of file
---
+++ matlab/PlottingTips.m
@@ -0,0 +1,112 @@
+%% Plotting Tips
+% This file collects some helpful tips for plotting in MATLAB. 
+%
+%
+%% Tip 1 - Adjust font sizes
+
+f = figure()
+subplot( 1, 3, 1 );
+t = 0:0.1:(2*pi);
+plot( t, sin( t ) );
+xlim( [0, 2*pi] );
+title( 'FontSize=10' );
+set( gca, 'fontsize', 10 );
+subplot( 1, 3, 2 );
+plot( t, sin( t ) );
+xlim( [0, 2*pi] );
+title( 'FontSize=14' );
+set( gca, 'fontsize', 14 );
+subplot( 1, 3, 3 );
+plot( t, sin( t ) );
+xlim( [0, 2*pi] );
+title( 'FontSize=18' );
+set( gca, 'fontsize', 18 );
+set( f, 'position', [808   742   988   219] );
+
+keyboard
+close( f )
+
+%% Tip 2 - Use different line widths
+
+f = figure();
+subplot( 1, 3, 1 );
+plot( 0:4, 0:4, 'linewidth', 1 );
+hold on;
+plot( 0:4, 2*(0:4), 'linewidth', 2 );
+plot( 0:4, 3*(0:4), 'linewidth', 3 );
+plot( 0:4, 4*(0:4), 'linewidth', 4 );
+plot( 0:4, 5*(0:4), 'linewidth', 5 );
+plot( 0:4, 6*(0:4), 'linewidth', 6 );
+hold off;
+xlim( [0, 4 ] );
+grid on;
+title( 'Adjust Linewidth' );
+set( gca, 'fontsize', 12 );
+subplot( 1, 3, 2 );
+t = randn( 100, 1000 );
+plot( t, 'linewidth', 0.2, 'color', [0.5 0.5 0.5] );
+hold on;
+plot( mean( t, 2 ), 'linewidth', 3, 'color', [0, 1, 0] );
+hold off;
+grid on;
+title( 'Statistics' );
+set( gca, 'fontsize', 12 );
+subplot( 1, 3, 3 );
+x = 0:6;
+y1 = 10 + -3*x;
+y2 = -10 + 3*x;
+z = 10*log10( 10.^(y1/10) + 10.^(y2/10) )
+plot( x, y1, 'r--', 'linewidth', 2 );
+hold on;
+plot( x, y2, 'g--', 'linewidth', 2 );
+plot( x, z, 'k', 'linewidth', 4 );
+hold off;
+title( 'Components' );
+legend( 'Comp 1', 'Comp 2', 'Total', 'location', 'north' );
+grid on;
+set( gca, 'fontsize', 12 );
+set( f, 'position', [808   742   988   219] );
+
+keyboard
+close( f )
+
+%% Tip 3 - Use multiple subplots
+
+f = figure()
+subplot( 2, 2, 1 );
+
+
+set( gca, 'fontsize', 14 );
+subplot( 2, 2, 2 );
+
+
+set( gca, 'fontsize', 14 );
+subplot( 2, 2, [ 3:4 ] );
+
+
+set( gca, 'fontsize', 14 );
+set( f, 'position', [916   526   866   420] );
+
+%% Tip 4 - Change the figure size
+
+
+
+%% Tip 5 - Plot vertical and horizontal lines
+
+
+%% Tip 6 - Manually set the legend position
+
+
+%% Tip 7 - Copy/paste lines onto the same plot
+
+
+%% Tip 8 - Use gcf and gca to match figure and axis properties
+
+
+%% Tip 9 - Use gco to adjust lines
+
+
+%% Tip 10 - Use findobj() to loop over plot objects
+
+
+%% end of file
