a8eca3f7d2734d8b83d9c690740780633d0ff84f
CJ Della Porta <cjdellaporta@gmail.com>
2020-07-12 15:27:42 -0400

    Update to legends. Checking in partial version of ArrayOrder code

---
+++ matlab/ArrayOrderAndPermute.m
@@ -0,0 +1,108 @@
+%% Array Order and Permuting
+% This tutorial discusses the order of arrays and how to efficiently loop over
+% data in MATLAB. In particular, the permute() function is introduced and used
+% to orient data to access any arbitrary dimensions. 
+%
+%% Array Order
+
+% When dealing with multi-dimensional or N-dimensional (ND) arrays it is 
+% important to be aware of how the data are stored in memory. This is referred 
+% to as array ordering. There are two general conventions: row-major and 
+% column-major ordering. These two different methods simply refer to which 
+% elements of the ND array area contiguous in memory, as well as, how such 
+% arrays should be linearly indexed. 
+
+% Here is a simple example of a 3 x 3 major in both conventions. So basically, 
+% we count elements down the columns for column-major and along the rows for
+% row-major. 
+colMaj = [ 1, 4, 7; 2, 5, 8; 3, 6, 9 ];
+disp( colMaj )
+rowMaj = [ 1:3; 4:6; 7:9 ];
+disp( rowMaj )
+keyboard();
+
+% We can trivially see how MATLAB stores the data by using the ':' operator, 
+% which will unwrap the data. Since MATLAB is column-major, you can see that
+% the colMaj matrix returns the index order.
+disp( colMaj(:) )
+disp( rowMaj(:) )
+keyboard();
+
+% To make this a little more clear, we can build a simple animation. Don't 
+% worry if you don't follow all of the plotting commands. 
+f = figure();
+set( f, 'units', 'inches' );
+set( f, 'position', [ 1, 1, 8, 6 ] );
+arrLeft = zeros( 1, 9 ); % linear array
+arrRight = zeros( 1, 9 ); % linear array
+cMap = cat( 1, [ 1 1 1 ], jet( 9 ) ); % common color map
+% - Column-major order matrix (left)
+a11 = subplot( 4, 2, [ 1, 3, 5] );
+imagesc( colMaj ); 
+set( a11, 'xticklabel', '' );
+set( a11, 'yticklabel', '' );
+axis( a11, 'equal' );
+colormap( a11, cMap( 2:end, : ) );
+title( 'Column-Major Order' );
+set( a11, 'fontsize', 16 );
+% - Row-major order matrix (right)
+a12 = subplot( 4, 2, [2, 4, 6] );
+imagesc( colMaj ); 
+set( a12, 'xticklabel', '' );
+set( a12, 'yticklabel', '' );
+axis( a12, 'equal' );
+colormap( a12, cMap( 2:end, : ) );
+title( 'Row-Major Order' );
+set( a12, 'fontsize', 16 );
+% - Linear array (left)
+a21 = subplot( 4, 2, 7 );
+hLeft = imagesc( arrLeft );
+caxis( a21, [0, 9] );
+colormap( a21, cMap );
+set( a21, 'xtick', 1:9 );
+set( a21, 'yticklabel', '' );
+axis( a21, 'equal' );
+set( a21, 'fontsize', 16 );
+% - Linear array (right)
+a22 = subplot( 4, 2, 8 );
+hRight = imagesc( arrRight );
+caxis( a22, [0, 9] );
+colormap( a22, cMap );
+set( a22, 'xtick', 1:9 );
+set( a22, 'yticklabel', '' );
+axis( a22, 'equal' );
+set( a22, 'fontsize', 16 );
+% - Animate everything
+nAnimations = 3
+for iView=1 : nAnimations
+
+  arrLeft = zeros( 1, 9 ); % reset to white
+  arrRight = zeros( 1, 9 ); % reset to white
+  [ anch1, anch2 ] = ndgrid( 0.5 : 1 : 2.5, 0.5 : 1 : 2.5 );
+  for ix1=1 : 9
+    keyboard;
+    pause( 1 );
+    arrLeft( 1, ix1, : ) = colMaj( ix1 );
+    arrRight( 1, ix1, : ) = rowMaj( ix1 );
+    set( hLeft, 'cdata', arrLeft );
+    set( hRight, 'cdata', arrRight );
+    if ix1 > 1
+      delete( hl1 );
+      delete( hl2 );
+    end
+    hl1 = rectangle( a11, 'position', [ anch2( ix1 ), anch1( ix1 ), 1, 1 ], ...
+      'edgecolor', 'r', 'linewidth', 2 );
+    hl2 = rectangle( a12, 'position', [ anch1( ix1 ), anch2( ix1 ), 1, 1 ], ...
+      'edgecolor', 'r', 'linewidth', 2 );
+    drawnow();
+  end
+  delete( hl1 );
+  delete( hl2 );
+
+end
+
+%% Looping over data
+
+
+
+%% end of file
--- matlab/Legends.m
+++ matlab/Legends.m
@@ -32,48 +32,72 @@ keyboard();
 % Another convenient way to set the legend strings using a cell array. This
 % is convenient when you are programatically creating your label strings.
 legStr = { 'Line 1', 'Line 2', 'Line 3' };
-legend( legStr{:}, 'location', 'north' );
+legend( legStr, 'location', 'north' );
 
 % Adjust the location of the legend
-legend( legStr{:}, 'location', 'north' );
+legend( legStr, 'location', 'north' );
 title( 'Location: north' );
 keyboard();
-legend( legStr{:}, 'location', 'northwest' );
+legend( legStr, 'location', 'northwest' );
 title( 'Location: northwest' );
 keyboard();
-legend( legStr{:}, 'location', 'west' );
+legend( legStr, 'location', 'west' );
 title( 'Location: west' );
 keyboard();
-legend( legStr{:}, 'location', 'southwest' );
+legend( legStr, 'location', 'southwest' );
 title( 'Location: southwest' );
 keyboard();
-legend( legStr{:}, 'location', 'south' );
+legend( legStr, 'location', 'south' );
 title( 'Location: south' );
 keyboard();
-legend( legStr{:}, 'location', 'southeast' );
+legend( legStr, 'location', 'southeast' );
 title( 'Location: southeast' );
 keyboard();
-legend( legStr{:}, 'location', 'east' );
+legend( legStr, 'location', 'east' );
 title( 'Location: east' );
 keyboard();
 
 % Move the legend outside
-legend( legStr{:}, 'location', 'eastoutside' );
+legend( legStr, 'location', 'eastoutside' );
 title( 'Location: eastoutside' );
 keyboard();
-legend( legStr{:}, 'location', 'westoutside' );
+legend( legStr, 'location', 'westoutside' );
 title( 'Location: westoutside' );
 keyboard();
 
 % Adjust the legend orientation
-legend( legStr{:}, 'orientation', 'horizontal', ...
+legend( legStr, 'orientation', 'horizontal', ...
   'location', 'southoutside' );
 title( 'Location: southoutside' );
 keyboard();
 
+close( f );
+
+%% Set legend properties manually
+% It is also useful to set the legend properties using set() and get().
+
+% Create a figure and save its handle. For consistancy, the figures will be 
+% sized in inches. 
+f = figure();
+set( f, 'units', 'inches' );
+pause( 0.1 );
+set( f, 'position', [1   1    5.8257    4.3693] );
+
+% Make a simple plot to demostrate the plot legend
+x = 0 : 0.1 : ( 2*pi );
+plot( x, sin( x ), 'rx', 'linewidth', 2 );
+hold on;
+plot( x, cos( x/3 ) - 0.25, 'bs', 'linewidth', 2 );
+plot( x, cos( 4*x )/3, '^g', 'linewidth', 2 );
+hold off;
+xlim( [ 0, 2*pi ] );
+grid on;
+title( 'Sample Plot' );
+set( gca, 'fontsize', 16 );
+
 % Use the legend handle to adjust settings. The legend properties can also be 
 % set using the handle. 
-hl = legend( legStr{:} );
+hl = legend( legStr );
 set( hl, 'orientation', 'vertical' );
 set( hl, 'location', 'northeast' );
 set( hl, 'fontsize', 16 );
@@ -82,6 +106,67 @@ title( 'Location: northwest' );
 keyboard();
 
 close( f )
+
+% Create a figure
+f = figure();
+set( f, 'units', 'inches' );
+pause( 0.1 );
+set( f, 'position', [ 1, 1, 6, 4 ] );
+
+% Set positions manually
+set( f, 'units', 'normalized' );
+a = subplot( 'position', [ 0.1, 0.1, 0.5, 0.8 ] );
+x = 0 : 0.1 : ( 2*pi );
+plot( x, sin( x ), 'rx', 'linewidth', 2 );
+hold on;
+plot( x, cos( x/3 ) - 0.25, 'bs', 'linewidth', 2 );
+plot( x, cos( 4*x )/3, '^g', 'linewidth', 2 );
+hold off;
+xlim( [ 0, 2*pi ] );
+grid on;
+title( 'Sample Plot' );
+set( a, 'fontsize', 16 );
+hl = legend( 'Line 1', 'Line 2', 'Line 3' );
+set( hl, 'fontsize', 16 );
+set( hl, 'position', [ 0.74   0.25   0.12721   0.14310 ] );
+a = uicontrol( f, 'units', 'normalized', 'position', [ 0.7, 0.7, 0.2, 0.1 ], ...
+  'style', 'pushbutton', 'string', 'Press Me' );
+a = uicontrol( f, 'units', 'normalized', 'position', [ 0.7, 0.5, 0.2, 0.1 ], ...
+  'style', 'pushbutton', 'string', 'Click Here' );
+
+keyboard();
+
+close( f );
+
+%% Set the line properties
+% Another to set the legend labels is to directly set the DisplayName property
+% on the individual lines. 
+
+% Create a figure and save its handle. For consistancy, the figures will be 
+% sized in inches. 
+f = figure();
+set( f, 'units', 'inches' );
+pause( 0.1 );
+set( f, 'position', [1   1    5.8257    4.3693] );
+
+% Make a simple plot to demostrate the plot legend
+x = 0 : 0.1 : ( 2*pi );
+plot( x, sin( x ), 'rx', 'linewidth', 2, 'DisplayName', 'Line1' );
+hold on;
+plot( x, cos( x/3 ) - 0.25, 'bs', 'linewidth', 2, 'DisplayName', 'Line2' );
+plot( x, cos( 4*x )/3, '^g', 'linewidth', 2, 'DisplayName', 'Line3' );
+hold off;
+xlim( [ 0, 2*pi ] );
+grid on;
+title( 'Sample Plot' );
+set( gca, 'fontsize', 16 );
+
+% Turn on the legend
+legend();
+
+keyboard();
+
+close( f );
 
 %% Add selective legends
 
@@ -113,39 +198,5 @@ set( hl, 'fontsize', 16 );
 keyboard();
 
 close( f );
-
-%% Set legend position manually
-% It is also useful to set the legend position
-
-% Create a figure
-f = figure();
-set( f, 'units', 'inches' );
-pause( 0.1 );
-set( f, 'position', [ 1, 1, 6, 4 ] );
-
-% Set positions manually
-set( f, 'units', 'normalized' );
-a = subplot( 'position', [ 0.1, 0.1, 0.5, 0.8 ] );
-x = 0 : 0.1 : ( 2*pi );
-plot( x, sin( x ), 'rx', 'linewidth', 2 );
-hold on;
-plot( x, cos( x/3 ) - 0.25, 'bs', 'linewidth', 2 );
-plot( x, cos( 4*x )/3, '^g', 'linewidth', 2 );
-hold off;
-xlim( [ 0, 2*pi ] );
-grid on;
-title( 'Sample Plot' );
-set( a, 'fontsize', 16 );
-hl = legend( 'Line 1', 'Line 2', 'Line 3' );
-set( hl, 'fontsize', 16 );
-set( hl, 'position', [ 0.74   0.25   0.12721   0.14310 ] );
-a = uicontrol( f, 'units', 'normalized', 'position', [ 0.7, 0.7, 0.2, 0.1 ], ...
-  'style', 'pushbutton', 'string', 'Press Me' );
-a = uicontrol( f, 'units', 'normalized', 'position', [ 0.7, 0.5, 0.2, 0.1 ], ...
-  'style', 'pushbutton', 'string', 'Click Here' );
-
-keyboard();
-
-close( f )
 
 %% end of file
